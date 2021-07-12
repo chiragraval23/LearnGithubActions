dc5df61a3a658408e7ac2f71a3b995d23daff279
chiragraval23 <@chirag2018>
2021-07-12 13:25:03 +0530

    Create init function

--- app/src/main/java/com/example/learngithubactions/MainActivity.kt
+++ app/src/main/java/com/example/learngithubactions/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initControls()
+    }
+
+    private fun initControls() {
+        TODO("Not yet implemented")
     }
 }
